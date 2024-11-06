73385581f9615a7a485d6f66433e76b470572f3c
USER04 <jabberwocker04@gmail.com>
2024-11-06 15:30:44 +0900

    Initial commit

---
+++ src/main/java/com/sparta/gitandrun/GitandrunApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.gitandrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitandrunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitandrunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/gitandrun/GitandrunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.gitandrun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitandrunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
