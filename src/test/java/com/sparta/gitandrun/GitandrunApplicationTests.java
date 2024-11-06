8d283d2f5eced2f56f6a9b59b2086a303280364f
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-11-06 17:32:45 +0900

    @SpringbootTest 임시 주석 처리

--- src/test/java/com/sparta/gitandrun/GitandrunApplicationTests.java
+++ src/test/java/com/sparta/gitandrun/GitandrunApplicationTests.java
@@ -3,7 +3,7 @@ package com.sparta.gitandrun;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class GitandrunApplicationTests {
 
     @Test
