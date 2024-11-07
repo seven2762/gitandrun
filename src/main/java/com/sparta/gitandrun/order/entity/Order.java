2bd42141e49429fbe0f56b38339fcd9876af126f
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-11-07 19:32:29 +0900

    [ feature ] BaseEntity 상속 및 OrderMenu와의 관계 추가 ( #2 )
    - Order 엔티티가 BaseEntity를 상속받아 공통 필드와 기능을 상속받도록 수정
    
    - OrderMenu와의 일대다 관계 추가: OrderMenu 엔티티와의 연관 관계를 설정하기 위해 `orderMenus` 필드 추가

--- src/main/java/com/sparta/gitandrun/order/entity/Order.java
+++ src/main/java/com/sparta/gitandrun/order/entity/Order.java
@@ -1,15 +1,21 @@
 package com.sparta.gitandrun.order.entity;
 
+import com.sparta.gitandrun.common.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.CascadeType.PERSIST;
+
 @Entity
 @Getter
-@Table(name = "orders")
+@Table(name = "p_order")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Order {
+public class Order extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +26,7 @@ public class Order {
     private OrderType orderType;
 
     private boolean isDeleted;
+
+    @OneToMany(mappedBy = "order", cascade = PERSIST, orphanRemoval = true)
+    private List<OrderMenu> orderMenus = new ArrayList<>();
 }
