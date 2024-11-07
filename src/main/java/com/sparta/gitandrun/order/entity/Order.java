508940f90e66e365acb95b2a97a60c41ffd4f0b0
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-11-07 18:44:49 +0900

    [ feature ] Order 엔티티 및 OrderType enum 클래스 생성 ( #2 )
    -  OrderType enum 클래스에서 DELIVERY와 VISIT 타입 정의
    
    - Order 엔티티에 id, orderType, isDeleted 필드 추가
    
    - 기본 생성자는 PROTECTED 접근 제한자로 설정

---
+++ src/main/java/com/sparta/gitandrun/order/entity/Order.java
@@ -0,0 +1,23 @@
+package com.sparta.gitandrun.order.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private OrderType orderType;
+
+    private boolean isDeleted;
+}
