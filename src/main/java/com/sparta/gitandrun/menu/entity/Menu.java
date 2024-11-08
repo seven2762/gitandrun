4b3b5d11ec1bc1fea154097d74795bccb42fa042
USER04 <jabberwocker04@gmail.com>
2024-11-08 16:17:41 +0900

    [ feature ]@SoftDelete, BaseEntity 적용

--- src/main/java/com/sparta/gitandrun/menu/entity/Menu.java
+++ src/main/java/com/sparta/gitandrun/menu/entity/Menu.java
@@ -1,11 +1,13 @@
 package com.sparta.gitandrun.menu.entity;
 
+import com.sparta.gitandrun.common.entity.BaseEntity;
 import com.sparta.gitandrun.menu.dto.MenuRequestDto;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SoftDelete;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
@@ -16,44 +18,43 @@ import java.time.LocalDateTime;
 @Setter
 @Table(name = "p_menu")
 @NoArgsConstructor
-@SQLDelete(sql = "UPDATE p_menu set is_deleted = true where id = ?")
-public class Menu extends BaseTimeEntity {
+@SoftDelete(columnName = "is_deleted")
+public class Menu extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(name = "menuName", nullable = false, length = 20)
     private String name;
 
-    @Column(nullable = false)
+    @Column(name = "menuPrice", nullable = false)
     private int price;
 
-    @Column(nullable = false)
+    @Column(name = "menuContent", nullable = false, length = 100)
     private String content;
 
-//    @Column(nullable = false)
-//    @CreatedDate
-//    private LocalDateTime createdAt;
+    @Column(nullable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
 
     @Column(nullable = false)
-    private String createBy;
+    private String createdBy;
 
-//    @Column(nullable = false)
-//    @LastModifiedDate
-//    private LocalDateTime updatedAt;
+    @Column(nullable = false, length = 100)
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 
     @Column(nullable = false)
-
     private String updatedBy;
 
-    @Column(nullable = false)
-    private Boolean isDeleted = Boolean.FALSE;
+    @Column(name = "is_deleted", nullable = false, insertable = false, updatable = false)
+    private boolean isDeleted;
 
     @Column(nullable = true)
     private LocalDateTime deletedAt;
 
-    @Column(nullable = true)
+    @Column(nullable = true, length = 100)
     private String deletedBy;
 
 /*  Store Entity 생성시 ManyToOne관계로 조인하여 연결
@@ -68,9 +69,9 @@ public class Menu extends BaseTimeEntity {
             this.name = requestDto.getName();
             this.price = requestDto.getPrice();
             this.content = requestDto.getContent();
-//            this.createdAt = requestDto.getCreatedAt();
-            this.createBy = "test created user";
-//            this.updatedAt = requestDto.getUpdatedAt();
+            this.createdAt = requestDto.getCreatedAt();
+            this.createdBy = "test created user";
+            this.updatedAt = requestDto.getUpdatedAt();
             this.updatedBy = "test updated user";
             this.isDeleted = Boolean.FALSE;
             this.deletedAt = requestDto.getDeletedAt();
@@ -81,6 +82,5 @@ public class Menu extends BaseTimeEntity {
         this.name = requestDto.getName();
         this.price = requestDto.getPrice();
         this.content = requestDto.getContent();
-        this.updatedAt = requestDto.getUpdatedAt();
     }
 }
