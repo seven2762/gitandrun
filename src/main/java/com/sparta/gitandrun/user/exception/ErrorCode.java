95baf72645145fb55f6015c97839badf729bc0f2
seven2762 <seven2762@gmail.com>
2024-11-09 21:05:33 +0900

    [feature][User] ErroCode enum 클래스 추가 (#8)

---
+++ src/main/java/com/sparta/gitandrun/user/exception/ErrorCode.java
@@ -0,0 +1,17 @@
+package com.sparta.gitandrun.user.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+    DUPLICATED_USER(400,"이미 가입된 회원 입니다."),
+    USER_NOT_FOUND(400, "회원을 찾을 수 없습니다");
+    private final int code;
+    private final String message;
+
+
+    ErrorCode(int num, String text) {
+        code = num;
+        message = text;
+    }
+}
