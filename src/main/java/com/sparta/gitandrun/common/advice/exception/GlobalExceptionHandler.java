a9a2b676ad6b95ba19909a59d9bb14d67bb1a0f5
soo1e <gameswuon@gmail.com>
2024-11-10 13:56:11 +0900

    예외 처리를 위한 익셉션 핸들러 추가

---
+++ src/main/java/com/sparta/gitandrun/common/advice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.sparta.gitandrun.common.advice.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j(topic = "GlobalExceptionHandler")
+public class GlobalExceptionHandler {
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<RestApiException> illegalArgumentExceptionHandler(IllegalArgumentException ex) {
+        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+        log.error(ex.getMessage());
+        return new ResponseEntity<>(
+                restApiException,
+                HttpStatus.BAD_REQUEST
+        );
+    }
+}
