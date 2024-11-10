b0870be19632cce79d801cec2217f2eb9f2f1bcf
Hujin0322 <hongyuj0322@naver.com>
2024-11-10 13:16:02 +0900

    [feature] review Entity 생성 ( #10 )

---
+++ src/main/java/com/sparta/gitandrun/Review/entity/Review.java
@@ -0,0 +1,77 @@
+package com.sparta.gitandrun.Review.entity;
+
+import com.sparta.gitandrun.order.entity.Order;
+import com.sparta.gitandrun.store.entity.Store;
+import com.sparta.gitandrun.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor
+@Table(name = "p_review")
+public class Review {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "review_id")
+    private UUID reviewId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user; //회원별 조회
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store; //가게별 조회
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order; //주문과 리뷰 연결
+
+    @Column(name = "review_content", nullable = false, length = 500)
+    private String reviewContent;
+
+    @Column(name = "review_rating", nullable = false)
+    private Short reviewRating;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "created_by", nullable = false, length = 30, updatable = false)
+    protected String createdBy;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "updated_by", nullable = false, length = 30)
+    protected String updatedBy;
+
+    @Column(name = "is_deleted", nullable = false)
+    private Boolean isDeleted = Boolean.FALSE;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Column(name = "deleted_by", length = 30)
+    private String deletedBy;
+}
