9af819ab8218b7d4ad86060f7505a71feb68f498
soo1e <gameswuon@gmail.com>
2024-11-13 00:49:30 +0900

    BaseEntity 1차 수정

--- src/main/java/com/sparta/gitandrun/common/entity/BaseEntity.java
+++ src/main/java/com/sparta/gitandrun/common/entity/BaseEntity.java
@@ -1,7 +1,10 @@
 package com.sparta.gitandrun.common.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -27,11 +30,15 @@ public class BaseEntity {
     @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime deletedAt;
 
-    @Column(nullable = false)
+    @Column
     private String createdBy;
 
-    @Column(nullable = false)
+    @Column
     private String updatedBy;
-
+    @Column
     private String deletedBy;
+
+    protected void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
 }
