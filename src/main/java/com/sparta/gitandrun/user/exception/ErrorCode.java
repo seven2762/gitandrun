7bb51978fe20cd4756cd89fff8e9c2b4c752d820
seven2762 <seven2762@gmail.com>
2024-11-14 03:09:55 +0900

    [feature][ErrorCode] 상수 추가 (비밀번호) (#31)

--- src/main/java/com/sparta/gitandrun/user/exception/ErrorCode.java
+++ src/main/java/com/sparta/gitandrun/user/exception/ErrorCode.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
     DUPLICATED_USER(400,"이미 가입된 회원 입니다."),
-    USER_NOT_FOUND(400, "회원을 찾을 수 없습니다");
+    USER_NOT_FOUND(400, "회원을 찾을 수 없습니다"),
+    INVALID_PASSWORD(400,"잘못된 비밀번호 입니다." );
+
     private final int code;
     private final String message;
 
