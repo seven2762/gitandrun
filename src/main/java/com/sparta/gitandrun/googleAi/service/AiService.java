fcd93b1f3d1a9b437ba85a345acbb44f422f1ad6
USER04 <jabberwocker04@gmail.com>
2024-11-14 02:54:24 +0900

    Ai 질답 구현(Raw Json 형태)

--- src/main/java/com/sparta/gitandrun/googleAi/service/AiService.java
+++ src/main/java/com/sparta/gitandrun/googleAi/service/AiService.java
@@ -1,38 +1,25 @@
 package com.sparta.gitandrun.googleAi.service;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sparta.gitandrun.googleAi.dto.AiDto;
-import com.sparta.gitandrun.googleAi.repository.AiRepository;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.*;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.RequestEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
+
 
 import java.io.IOException;
-import java.net.URI;
-import java.util.List;
+
 
 @Service
 @Slf4j
 public class AiService {
 
-
     @Value("${google-api-key}")
     private String SECRET_KEY;
 
-    String projectId = "gen-lang-client-0589077725";
-
     private final OkHttpClient httpClient = new OkHttpClient();
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
     //CREATE
     public String createQuestion(String text) throws IOException {
 
@@ -52,7 +39,6 @@ public class AiService {
         contents.put(jsonObject1);
         jsonObject0.put("contents",contents);
 
-
         System.out.println(jsonObject0);
 
         RequestBody body = RequestBody.create(
@@ -64,14 +50,11 @@ public class AiService {
                 .post(body)
                 .build();
 
-
-        try (Response response = httpClient.newCall(request).execute()) {
-            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-            JsonNode jsonResponse = objectMapper.readTree(response.body().string());
-            return jsonResponse.path("response").path("text").asText();
-
-
-        }
+            Response response = httpClient.newCall(request).execute();
+//        try (Response response = httpClient.newCall(request).execute()) {
+//            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+            return response.body().string();
+//        }
 
     }
 }
