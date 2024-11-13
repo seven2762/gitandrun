56dc323ebf0f14cc10b7bbd3713129ad72044233
Hujin0322 <hongyuj0322@naver.com>
2024-11-14 08:54:10 +0900

    [feature] OrderRepository에 주문별 주문 상태 확인 위한 메서드 추가 ( #23 )
    - 특정 주문에 주문 상태가 완료인지 확인하는 findByIdAndOrderStatus 메서드 추가

--- src/main/java/com/sparta/gitandrun/order/repository/OrderRepository.java
+++ src/main/java/com/sparta/gitandrun/order/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package com.sparta.gitandrun.order.repository;
 
 import com.sparta.gitandrun.order.entity.Order;
+import com.sparta.gitandrun.order.entity.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,4 +15,7 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @Query("select o from Order o where o.user.userId = :userId and o.isDeleted = false ")
     List<Order> findByUserId(@Param("userId") Long userId);
+
+    //리뷰에서 orderId와 orderStatus 확인하기 위해 추가
+    Optional<Order> findByIdAndOrderStatus(Long orderId, OrderStatus orderStatus);
 }
