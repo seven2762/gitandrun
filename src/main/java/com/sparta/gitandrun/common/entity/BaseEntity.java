9276c4e1ea0ae03df41254a6335edb9367ee7f0e
seven2762 <seven2762@gmail.com>
2024-11-14 03:09:13 +0900

    [feature][BaseEntity] 사용하지 않는 import 삭제 (#31)

--- src/main/java/com/sparta/gitandrun/common/entity/BaseEntity.java
+++ src/main/java/com/sparta/gitandrun/common/entity/BaseEntity.java
@@ -1,10 +1,7 @@
 package com.sparta.gitandrun.common.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
