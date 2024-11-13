50a638ea68df706c60e43890bb64d920f68bd098
Hujin0322 <hongyuj0322@naver.com>
2024-11-13 19:59:11 +0900

    [refactor] 필요없는 라이브러리 정리 ( #23 )

--- src/main/java/com/sparta/gitandrun/Review/entity/Review.java
+++ src/main/java/com/sparta/gitandrun/Review/entity/Review.java
@@ -8,8 +8,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
