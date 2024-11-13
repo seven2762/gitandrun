9ccfdc04c5147ad17b914b3590527615b668c014
Hujin0322 <hongyuj0322@naver.com>
2024-11-14 08:52:21 +0900

    [feature] ReviewRepository에 주문별 리뷰 중복 작성 확인 메서드 추가 ( #23 )
    - 특정 주문에 이미 작성된 리뷰가 있는지 확인하는 existsByOrderId 메서드 추가

--- src/main/java/com/sparta/gitandrun/Review/repository/ReviewRepository.java
+++ src/main/java/com/sparta/gitandrun/Review/repository/ReviewRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface ReviewRepository extends JpaRepository<Review, UUID> {
+
+    // 해당 주문에 리뷰가 이미 존재하는지 확인
+    boolean existsByOrderId(Long orderId);
 }
