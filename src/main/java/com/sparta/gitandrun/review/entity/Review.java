18eb33848c6745cb70c0730aec415c4129c5bfb7
Hujin0322 <hongyuj0322@naver.com>
2024-11-15 02:44:04 +0900

    [refactor] Review entity 코드 정리 ( #49 )
    - BaseEntity 클래스 상속 받음
    - @softDelete 대신 삭제 시 직접 true로 변경

--- src/main/java/com/sparta/gitandrun/review/entity/Review.java
+++ src/main/java/com/sparta/gitandrun/review/entity/Review.java
@@ -1,36 +1,28 @@
 package com.sparta.gitandrun.review.entity;
 
+import com.sparta.gitandrun.common.entity.BaseEntity;
 import com.sparta.gitandrun.review.dto.ReviewRequestDto;
 import com.sparta.gitandrun.order.entity.Order;
 import com.sparta.gitandrun.user.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
-import jakarta.persistence.PreRemove;
 import jakarta.persistence.Table;
-import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.SoftDelete;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
 @Setter
-@EntityListeners(AuditingEntityListener.class)
-@SoftDelete(columnName = "is_deleted")
 @NoArgsConstructor
 @Table(name = "p_review")
-public class Review {
+public class Review extends BaseEntity {
 
     @Id
     @Column(name = "review_id")
@@ -53,35 +45,8 @@ public class Review {
     @Column(name = "review_rating", nullable = false)
     private Short reviewRating;
 
-    @CreatedDate
-    @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
-
-    @Column(name = "created_by", nullable = false, length = 30, updatable = false)
-    protected String createdBy;
-
-    @LastModifiedDate
-    @Column(name = "updated_at", nullable = false)
-    private LocalDateTime updatedAt;
-
-    @Column(name = "updated_by", nullable = false, length = 30)
-    protected String updatedBy;
-
-    @Column(name = "is_deleted", insertable = false, updatable = false)
-    private boolean isDeleted;
-
-    @Column(name = "deleted_at")
-    private LocalDateTime deletedAt;
-
-    @Column(name = "deleted_by", length = 30)
-    private String deletedBy;
-
-    @PreRemove
-    public void preRemove() {
-        this.isDeleted = true;
-        this.deletedAt = LocalDateTime.now();
-        this.deletedBy = String.valueOf(user.getUserId());
-    }
+    @Column(name = "is_deleted")
+    private boolean isDeleted = false;
 
     public Review(ReviewRequestDto requestDto, User user, UUID storeId, Order order) {
         this.user = user;
@@ -89,7 +54,5 @@ public class Review {
         this.order = order;
         this.reviewContent = requestDto.getReviewContent();
         this.reviewRating = requestDto.getReviewRating();
-        this.createdBy = String.valueOf(user.getUserId());
-        this.updatedBy = String.valueOf(user.getUserId());
     }
 }
