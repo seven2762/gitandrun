870798f8f56742c5229f4e90b431e676fe788221
Hujin0322 <162073663+Hujin0322@users.noreply.github.com>
2024-11-14 10:43:18 +0900

    Update OrderRepository.java

--- src/main/java/com/sparta/gitandrun/order/repository/OrderRepository.java
+++ src/main/java/com/sparta/gitandrun/order/repository/OrderRepository.java
@@ -14,9 +14,6 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     @Query("select o from Order o where o.id = :orderId and o.isDeleted = false ")
     Optional<Order> findOrderById(@Param("orderId") Long orderId);
 
-    @Query("select o from Order o where o.user.userId = :userId and o.isDeleted = false ")
-    List<Order> findByUserId(@Param("userId") Long userId);
-
     //리뷰에서 orderId와 orderStatus 확인하기 위해 추가
     Optional<Order> findByIdAndOrderStatus(Long orderId, OrderStatus orderStatus);
   
