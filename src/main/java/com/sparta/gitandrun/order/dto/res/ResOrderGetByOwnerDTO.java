baa707e27a98a518b9a34d8670d011c43d054f6c
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-11-15 00:17:49 +0900

    [ feature ] 사장 권한 주문 페이지 조회 DTO 구현 ( #17 )
     - 주문 페이지와 고객 정보를 포함하여 Owner가 조회할 수 있는 주문 페이지 응답 DTO 작성

---
+++ src/main/java/com/sparta/gitandrun/order/dto/res/ResOrderGetByOwnerDTO.java
@@ -0,0 +1,142 @@
+package com.sparta.gitandrun.order.dto.res;
+
+import com.sparta.gitandrun.order.entity.Order;
+import com.sparta.gitandrun.order.entity.OrderMenu;
+import com.sparta.gitandrun.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.web.PagedModel;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResOrderGetByOwnerDTO {
+
+    private OrderPage orderPage;
+
+    public static ResOrderGetByOwnerDTO of(Page<Order> orderPage, List<OrderMenu> orderMenus) {
+        return ResOrderGetByOwnerDTO.builder()
+                .orderPage(new ResOrderGetByOwnerDTO.OrderPage(orderPage, orderMenus))
+                .build();
+    }
+
+    @Getter
+    private static class OrderPage extends PagedModel<OrderPage.OrderDTO> {
+
+        public OrderPage(Page<Order> orderPage, List<OrderMenu> orderMenus) {
+            super(
+                    new PageImpl<>(
+                            OrderDTO.from(orderPage.getContent(), orderMenus),
+                            orderPage.getPageable(),
+                            orderPage.getTotalElements()
+                    )
+            );
+        }
+
+        @Getter
+        @Builder
+        @NoArgsConstructor
+        @AllArgsConstructor
+        private static class OrderDTO {
+            private Long orderId;
+            private String status;
+            private String type;
+            private Customer customer;
+            private List<OrderMenuDTO> orderMenuDTOS;
+            private int totalPrice;
+
+            public static List<OrderDTO> from(List<Order> orders, List<OrderMenu> orderMenus) {
+                return orders.stream()
+                        .map(order -> from(order, orderMenus))
+                        .toList();
+            }
+
+            public static OrderDTO from(Order order, List<OrderMenu> orderMenus) {
+
+                List<OrderDTO.OrderMenuDTO> orderMenuDTOS = OrderMenuDTO.from(orderMenus).get(order.getId());
+
+                return OrderDTO.builder()
+                        .orderId(order.getId())
+                        .status(order.getOrderStatus().status)
+                        .type(order.getOrderType().getType())
+                        .orderMenuDTOS(orderMenuDTOS)
+                        .customer(Customer.from(order.getUser()))
+                        .totalPrice(sumFrom(orderMenuDTOS))
+                        .build();
+            }
+
+            private static int sumFrom(List<OrderMenuDTO> orderMenuDTOS) {
+                return orderMenuDTOS.stream()
+                        .mapToInt(OrderMenuDTO::getMenuPrice)
+                        .sum();
+            }
+
+            @Getter
+            @Builder
+            @NoArgsConstructor
+            @AllArgsConstructor
+            private static class Customer {
+                private long userId;
+                private String nickName;
+                private String phone;
+                private String zipcode;
+                private String address;
+                private String addressDetail;
+
+                private static Customer from(User user) {
+                    return Customer.builder()
+                            .userId(user.getUserId())
+                            .nickName(user.getNickName())
+                            .phone(user.getPhone())
+                            .zipcode(user.getAddress().getZipCode())
+                            .zipcode(user.getAddress().getAddress())
+                            .zipcode(user.getAddress().getAddressDetail())
+                            .build();
+                }
+            }
+
+            @Getter
+            @Builder
+            @NoArgsConstructor
+            @AllArgsConstructor
+            private static class OrderMenuDTO {
+                private Long orderMenuId;
+                private UUID menuId;
+                private String menuName;
+                private int menuPrice;
+                private int count;
+
+                private static Map<Long, List<OrderMenuDTO>> from(List<OrderMenu> orderMenus) {
+                    return orderMenus.stream()
+                            .collect(Collectors.groupingBy(
+                                    orderMenu -> orderMenu.getOrder().getId(),
+                                    Collectors.mapping(
+                                            OrderMenuDTO::from,
+                                            Collectors.toList()
+                                    )
+                            ));
+                }
+
+                public static OrderMenuDTO from(OrderMenu orderMenu) {
+                    return OrderMenuDTO.builder()
+                            .orderMenuId(orderMenu.getId())
+                            .menuId(orderMenu.getMenu().getMenuId())
+                            .menuName(orderMenu.getMenu().getMenuName())
+                            .menuPrice(orderMenu.getOrderPrice())
+                            .count(orderMenu.getOrderCount())
+                            .build();
+                }
+            }
+        }
+    }
+}
