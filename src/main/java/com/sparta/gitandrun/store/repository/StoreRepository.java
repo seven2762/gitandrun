a2095e0d7c8386642dea587381af3cdb4cdf4f3b
soo1e <gameswuon@gmail.com>
2024-11-15 17:14:04 +0900

    메서드 생성 및 정리

--- src/main/java/com/sparta/gitandrun/store/repository/StoreRepository.java
+++ src/main/java/com/sparta/gitandrun/store/repository/StoreRepository.java
@@ -13,23 +13,27 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface StoreRepository extends JpaRepository<Store, UUID> {
-    List<Store> findByisDeletedFalse();
+    List<Store> findByIsDeletedFalse();
     Optional<Store> findById(UUID storeId); // Store의 ID로 조회하는 메서드
     List<Store> findByUser_UserId(Long userId);
 
-    @Query("SELECT s FROM Store s WHERE s.storeName LIKE %:keyword% OR s.address.address LIKE %:keyword%")
-    Page<Store> searchStores(@Param("keyword") String keyword, Pageable pageable);
-
+    @Query("SELECT s FROM Store s WHERE s.category.id = :categoryId")
     Page<Store> findByCategoryId(UUID categoryId, Pageable pageable);
 
-    @Query("SELECT s FROM Store s WHERE s.storeName LIKE %:keyword% OR s.address.address LIKE %:keyword%")
-    Page<Store> searchByKeyword(@Param("keyword") String keyword, Pageable pageable);
+    @Query("SELECT s FROM Store s WHERE s.category.id = :categoryId AND s.isDeleted = false")
+    Page<Store> findByCategoryIdAndIsDeletedFalse(UUID categoryId, Pageable pageable);
 
-    // 지역 이름으로 가게 검색
+    @Query("SELECT s FROM Store s WHERE s.storeName LIKE %:keyword% OR s.phone LIKE %:keyword%")
+    Page<Store> searchStores(@Param("keyword") String keyword, Pageable pageable);
+
     @Query("SELECT s FROM Store s WHERE s.region.regionName LIKE %:regionName%")
     List<Store> findByRegionName(@Param("regionName") String regionName);
 
-    // 페이징 처리와 함께 지역 이름으로 가게 검색
-    @Query("SELECT s FROM Store s WHERE s.region.regionName LIKE %:regionName%")
-    Page<Store> findByRegionNameWithPagination(@Param("regionName") String regionName, Pageable pageable);
+    @Query("SELECT s FROM Store s WHERE " +
+            "(LOWER(s.storeName) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+            "OR LOWER(s.address.address) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+            "OR LOWER(s.address.addressDetail) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
+            "OR LOWER(s.category.name) LIKE LOWER(CONCAT('%', :keyword, '%'))) " +
+            "AND s.isDeleted = false")
+    Page<Store> searchStoresAndIsDeletedFalse(@Param("keyword") String keyword, Pageable pageable);
 }
