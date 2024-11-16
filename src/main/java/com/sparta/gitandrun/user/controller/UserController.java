6a31fb6c5762d32b66afa7e57cfbef62eaff3524
seven2762 <seven2762@gmail.com>
2024-11-16 17:58:54 +0900

    [feature][User] 페이징 컨트롤러 추가 , 회원가입 컨트롤러 @Valid 어노테이션 추가 , 기존 회원 검색 컨틀롤러 삭제 (#81)

--- src/main/java/com/sparta/gitandrun/user/controller/UserController.java
+++ src/main/java/com/sparta/gitandrun/user/controller/UserController.java
@@ -3,20 +3,24 @@ package com.sparta.gitandrun.user.controller;
 
 import com.sparta.gitandrun.common.entity.ApiResDto;
 import com.sparta.gitandrun.user.dto.request.SignUpReqDTO;
-import com.sparta.gitandrun.user.dto.response.SignUpResDTO;
-import com.sparta.gitandrun.user.dto.response.UserApiResDto;
-import com.sparta.gitandrun.user.dto.response.UserResDTO;
+import com.sparta.gitandrun.user.dto.request.UserSearchReqDto;
+import com.sparta.gitandrun.user.dto.response.*;
 import com.sparta.gitandrun.user.entity.User;
 import com.sparta.gitandrun.user.security.UserDetailsImpl;
 import com.sparta.gitandrun.user.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
-
+@Slf4j
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
@@ -25,17 +29,11 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<UserApiResDto> signup(@RequestBody SignUpReqDTO signUpReqDTO) {
+    public ResponseEntity<UserApiResDto> signup(@Valid @RequestBody SignUpReqDTO signUpReqDTO) {
         SignUpResDTO signUpResDTO = userService.signUp(signUpReqDTO);
         return ResponseEntity.ok(new UserApiResDto("회원 가입이 완료되었습니다.", 200, signUpResDTO));
     }
 
-
-    @GetMapping("/all")
-    public ResponseEntity<UserApiResDto> getAllUsers() {
-        List<UserResDTO> users = userService.getAllActiveUsers();
-        return ResponseEntity.ok(new UserApiResDto("회원 목록 조회 성공", 200, users));
-    }
 
     @PutMapping("/password")
     public ResponseEntity<ApiResDto> updatePassword(@AuthenticationPrincipal UserDetailsImpl userDetails,   @RequestBody Map<String, String> request) {
@@ -50,4 +48,13 @@ public class UserController {
         return ResponseEntity.ok(new ApiResDto("회원 소프트 딜리트 완료", 200));
     }
 
+    @Secured({"ROLE_ADMIN", "ROLE_MANAGER"})
+    @GetMapping("/list")
+    public ResponseEntity<PageResDto<PagedUserResponseDTO>> getUsers(
+            @ModelAttribute UserSearchReqDto searchDto,
+            @RequestParam(defaultValue = "10") int size,
+            @PageableDefault(sort = {"createdAt", "modifiedAt"}, direction = Sort.Direction.DESC) Pageable pageable) {
+
+        return userService.getPagedUsers(searchDto, pageable, size);
+    }
 }
