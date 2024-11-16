1de719c128d44480435289ab12f73b5c8f522e26
Hujin0322 <hongyuj0322@naver.com>
2024-11-17 00:47:16 +0900

    [feature] 관리자용 응답 데이터를 위한 AdminReviewResponseDto 생성 ( #70 )

---
+++ src/main/java/com/sparta/gitandrun/review/dto/AdminReviewResponseDto.java
@@ -0,0 +1,42 @@
+package com.sparta.gitandrun.review.dto;
+
+import com.sparta.gitandrun.review.entity.Review;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class AdminReviewResponseDto {
+
+    private UUID reviewId;
+    private Long userId;
+    private Long orderId;
+    private UUID storeId;
+    private String reviewContent;
+    private Short reviewRating;
+    private String createdBy;
+    private LocalDateTime createdAt;
+    private String updatedBy;
+    private LocalDateTime updatedAt;
+    private boolean isDeleted;
+    private String deletedBy;
+    private LocalDateTime deletedAt;
+
+    public AdminReviewResponseDto(Review review) {
+        this.reviewId = review.getReviewId();
+        this.userId = review.getUser().getUserId();
+        this.orderId = review.getOrder().getId();
+        this.storeId = review.getStoreId();
+        this.reviewContent = review.getReviewContent();
+        this.reviewRating = review.getReviewRating();
+        this.createdBy = review.getCreatedBy();
+        this.createdAt = review.getCreatedAt();
+        this.updatedBy = review.getUpdatedBy();
+        this.updatedAt = review.getUpdatedAt();
+        this.isDeleted = review.isDeleted();
+        this.deletedBy = review.getDeletedBy();
+        this.deletedAt = review.getDeletedAt();
+    }
+}
