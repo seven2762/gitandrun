732a1981db9eb7ca913978650963f093d10bd1a0
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-11-16 22:29:40 +0900

    [ feature ] 고객 본인 결제 목록 조회 기능 구현 ( #82 )
    - 인증된 정보를 통해 본인의 결제 목록을 조회한다.
    
    - 결제 조회 간 연관관계에 있는 Order, Store fetch join 으로 한 번에 조회

--- src/main/java/com/sparta/gitandrun/payment/repository/PaymentRepository.java
+++ src/main/java/com/sparta/gitandrun/payment/repository/PaymentRepository.java
@@ -1,6 +1,8 @@
 package com.sparta.gitandrun.payment.repository;
 
 import com.sparta.gitandrun.payment.entity.Payment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,4 +16,10 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
     @Query("select p from Payment p where p.id = :paymentId and p.user.userId = :userId and p.paymentStatus = 'PAID'")
     Optional<Payment> findPaidPaymentByIdAndUserId(@Param("paymentId") Long paymentId, @Param("userId") Long userId);
+
+    @Query("select p from Payment p " +
+            "join fetch p.order o " +
+            "join fetch o.store " +
+            "where p.user.userId = :userId and p.paymentStatus = 'PAID' or p.paymentStatus = 'CANCEL'")
+    Page<Payment> findPaidPaymentPageByUserId(@Param("userId") Long userId, Pageable pageable);
 }
