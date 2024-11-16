6de93b46a315fc5cc2a6d06bdf1a19befa11f896
seven2762 <seven2762@gmail.com>
2024-11-16 17:57:49 +0900

    [feature][User] 회원가입 요청 DTO에 유효성 검증 추가 (#81)

--- src/main/java/com/sparta/gitandrun/user/dto/request/SignUpReqDTO.java
+++ src/main/java/com/sparta/gitandrun/user/dto/request/SignUpReqDTO.java
@@ -2,6 +2,7 @@ package com.sparta.gitandrun.user.dto.request;
 
 import com.sparta.gitandrun.user.dto.AddressDTO;
 import com.sparta.gitandrun.user.entity.Role;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,9 +13,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignUpReqDTO {
+    @Pattern(
+            regexp = "^[a-z0-9]{4,10}$",
+            message = "아이디는 4~10자의 알파벳 소문자와 숫자로만 구성되어야 합니다."
+    )
     private String username;
+
     private String nickName;
     private String email;
+
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,15}$",
+            message = "비밀번호는 8~15자의 알파벳 대소문자, 숫자, 특수문자를 포함해야 합니다."
+    )
     private String password;
     private Role role;
     private AddressDTO addressReq;
