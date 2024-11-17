ab279a5910c3d1feac302f33a04cca59d2b1fc32
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-11-18 01:15:50 +0900

    [ feature ] 각 enum 클래스에 문자열을 기반으로 Enum 값을 반환하는 fromString 메서드 추가 ( #100 )

--- src/main/java/com/sparta/gitandrun/order/entity/OrderType.java
+++ src/main/java/com/sparta/gitandrun/order/entity/OrderType.java
@@ -1,5 +1,10 @@
 package com.sparta.gitandrun.order.entity;
 
+import lombok.Getter;
+
+import java.util.Objects;
+
+@Getter
 public enum OrderType {
     DELIVERY(Type.DELIVERY),
     VISIT(Type.VISIT);
@@ -10,12 +15,14 @@ public enum OrderType {
         this.type = type;
     }
 
-    public String getType() {
-        return this.type;
-    }
-
     public static class Type {
         public static final String DELIVERY = "DELIVERY";
         public static final String VISIT = "VISIT";
+    }
+
+    public static OrderType fromString(String type) {
+        if (type == null) return DELIVERY;
+        return Objects.equals(type.toUpperCase(), OrderType.DELIVERY.getType())
+                ? DELIVERY : VISIT;
     }
 }
