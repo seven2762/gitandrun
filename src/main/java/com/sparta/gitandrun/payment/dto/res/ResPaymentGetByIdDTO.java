00bbee1d9075f181e1d888d7a39b02e07f470eb0
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-11-17 11:38:01 +0900

    [ feature ] ResPaymentGetByIdDTO 클래스 추가 ( #92 )
    - ResPaymentGetByIdDTO 클래스:
      - 결제 ID를 기반으로 결제 정보를 응답.
      - Payment, Order, OrderMenu 를 파라미터로 받음.
    
    - PaymentDTO 클래스:
      - id, 결제 금액(paymentPrice), 결제 상태(paymentStatus), 생성일(createdAt), 취소일(canceledAt) 필드 포함.
      - OrderDTO를 통해 주문 정보를 출력.
    
      - OrderDTO 클래스:
        - id, 주문 상태(status), 주문 타입(type), 생성일(createdAt) 필드 포함.
        - OrderMenuDTO 리스트 및 StoreDTO 정보를 출력
    
    - OrderMenuDTO 클래스:
      - id, 메뉴 ID(menuId), 메뉴 이름(menuName), 메뉴 가격(menuPrice), 수량(count) 필드 포함.
      - OrderMenu 리스트를 Map<Long, List<OrderMenuDTO>> 형태로 변환.
    
    - StoreDTO 클래스:
      - id, 우편번호(zipcode), 주소(address), 상세 주소(addressDetail) 필드 포함.

---
+++ src/main/java/com/sparta/gitandrun/payment/dto/res/ResPaymentGetByIdDTO.java
@@ -0,0 +1,140 @@
+package com.sparta.gitandrun.payment.dto.res;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.sparta.gitandrun.order.entity.Order;
+import com.sparta.gitandrun.order.entity.OrderMenu;
+import com.sparta.gitandrun.payment.entity.Payment;
+import com.sparta.gitandrun.payment.entity.enums.PaymentStatus;
+import com.sparta.gitandrun.store.entity.Store;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResPaymentGetByIdDTO {
+
+    private PaymentDTO paymentDTO;
+
+    public static ResPaymentGetByIdDTO of(Payment payment, Order order, List<OrderMenu> orderMenus) {
+        return ResPaymentGetByIdDTO.builder()
+                .paymentDTO(PaymentDTO.from(payment, order, orderMenus))
+                .build();
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private static class PaymentDTO {
+        private Long id;
+        private int paymentPrice;
+        private String paymentStatus;
+        private LocalDateTime createdAt;
+        private LocalDateTime canceledAt;
+        private OrderDTO orderDTO;
+
+
+        private static PaymentDTO from(Payment payment, Order order, List<OrderMenu> orderMenus) {
+            return PaymentDTO.builder()
+                    .id(payment.getId())
+                    .paymentPrice(payment.getPaymentPrice())
+                    .paymentStatus(payment.getPaymentStatus().status)
+                    .createdAt(payment.getCreatedAt())
+                    .canceledAt(
+                            payment.getPaymentStatus() == PaymentStatus.CANCEL
+                                    ? payment.getUpdatedAt() : null
+                    )
+                    .orderDTO(OrderDTO.from(order, orderMenus))
+                    .build();
+        }
+
+        @Getter
+        @Builder
+        @NoArgsConstructor
+        @AllArgsConstructor
+        private static class OrderDTO {
+            private Long id;
+            private String status;
+            private String type;
+            private LocalDateTime createdAt;
+            private List<OrderMenuDTO> orderMenuDTOS;
+            private StoreDTO storeDTO;
+
+            private static OrderDTO from(Order order, List<OrderMenu> orderMenus) {
+
+                return OrderDTO.builder()
+                        .id(order.getId())
+                        .status(order.getOrderStatus().status)
+                        .type(order.getOrderType().getType())
+                        .createdAt(order.getCreatedAt())
+                        .orderMenuDTOS(OrderMenuDTO.from(orderMenus).get(order.getId()))
+                        .storeDTO(StoreDTO.from(order.getStore()))
+                        .build();
+            }
+
+            @Getter
+            @Builder
+            @NoArgsConstructor
+            @AllArgsConstructor
+            private static class OrderMenuDTO {
+                private Long id;
+                private UUID menuId;
+                private String menuName;
+                private int menuPrice;
+                private int count;
+
+                private static Map<Long, List<OrderMenuDTO>> from(List<OrderMenu> orderMenus) {
+                    return orderMenus.stream()
+                            .collect(Collectors.groupingBy(
+                                    orderMenu -> orderMenu.getOrder().getId(),
+                                    Collectors.mapping(
+                                            OrderMenuDTO::from,
+                                            Collectors.toList(
+                                            )
+                                    )));
+                }
+
+                private static OrderMenuDTO from(OrderMenu orderMenu) {
+                    return OrderMenuDTO.builder()
+                            .id(orderMenu.getId())
+                            .menuId(orderMenu.getMenu().getMenuId())
+                            .menuName(orderMenu.getMenu().getMenuName())
+                            .menuPrice(orderMenu.getOrderPrice())
+                            .count(orderMenu.getOrderCount())
+                            .build();
+                }
+            }
+
+            @Getter
+            @Builder
+            @NoArgsConstructor
+            @AllArgsConstructor
+            private static class StoreDTO {
+                private UUID id;
+                private String zipcode;
+                private String address;
+                private String addressDetail;
+
+                private static StoreDTO from(Store store) {
+                    return StoreDTO.builder()
+                            .id(store.getStoreId())
+                            .zipcode(store.getAddress().getZipCode())
+                            .address(store.getAddress().getAddress())
+                            .addressDetail(store.getAddress().getAddressDetail())
+                            .build();
+                }
+            }
+        }
+    }
+}
