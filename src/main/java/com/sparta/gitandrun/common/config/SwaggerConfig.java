b2f373d58396938145a40a1b834ef72b46e1c996
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-11-18 22:12:08 +0900

    [ refactor ] Swagger 적용 ( #121 )

---
+++ src/main/java/com/sparta/gitandrun/common/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.sparta.gitandrun.common.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Payment API",
+                version = "v1",
+                description = "Payment 관련 API",
+                contact = @Contact(name = "Developer", email = "developer@example.com")
+        )
+)
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        // Security Requirement 추가
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("Authorization");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("Authorization", securityScheme))
+                .addSecurityItem(securityRequirement);
+    }
+
+}
