166fc4d1b3366f3bc2ae069513bdac3ccdfdbd2e
Hujin0322 <hongyuj0322@naver.com>
2024-11-18 18:34:19 +0900

    [refactor] 필요없는 라이브러리 삭제 ( #120 )

--- src/main/java/com/sparta/gitandrun/review/dto/ReviewRequestDto.java
+++ src/main/java/com/sparta/gitandrun/review/dto/ReviewRequestDto.java
@@ -2,8 +2,6 @@ package com.sparta.gitandrun.review.dto;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
